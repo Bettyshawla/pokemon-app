b0d788335fa28382ca773490d129fba7f900bf77
BettyShawlagit config --global user.email Betty.tada1@gmail.com <betty.tada1@gmail.com>
2022-11-17 22:43:06 -0500

    My server is set up and running

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+
+app.get('/', (req, res)=>{
+    res.send("Welcome to the Pokemon App!")
+})
+
+
+
+
+
+
+app.listen(PORT, () => {
+console.log(`Listening on port: ${PORT}`)
+})
