66545176d8a69647e8f686fa85f087badde695e9
BettyShawlagit config --global user.email Betty.tada1@gmail.com <betty.tada1@gmail.com>
2022-11-17 22:51:45 -0500

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
-
+const pokemon = require("./models/pokemon")
 
 app.get('/', (req, res)=>{
     res.send("Welcome to the Pokemon App!")
 })
 
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon)
+})
 
 
 
