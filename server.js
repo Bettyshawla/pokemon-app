8d70e020a622d58c16b051a8cf777bcf8c996a75
BettyShawlagit config --global user.email Betty.tada1@gmail.com <betty.tada1@gmail.com>
2022-11-19 19:10:08 -0500

    I can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -7,23 +7,32 @@ const reactViews = require("express-react-views")
 app.set("view engine", "jsx")
 app.engine("jsx", reactViews.createEngine())
 
-
+// localhost
 app.get('/', (req, res)=>{
     res.send("Welcome to the Pokemon App!")
 })
 
-// app.get('/pokemon', (req, res) => {
-//     res.send(pokemon)
-// })
-
-
+//  index.jsx
 app.get("/pokemon", (req, res) => {
     res.render("Index", {pokemon: pokemon})
 })
 
 
 
+// app.get('/pokemon/:indexOfPokemonArray', (req, res) => {
+//     res.send(pokemon[req.params.indexOfPokemonArray]);
+// })
 
+
+
+
+
+
+
+
+
+
+// listening port
 app.listen(PORT, () => {
 console.log(`Listening on port: ${PORT}`)
 })
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,19 +1,42 @@
 const myStyle = {
     color: '#ffffff',
     backgroundColor: '#000000',
-    };
+};
 
-const React = require("React")
+const React = require("react")
 
 class Index extends React.Component {
     render() {
         const { pokemon } = this.props
-            return(
-                <div>
-                    <h1 style={myStyle}>See All The Pokemon!</h1>
+        return (
+            <div>
+                <h1 style={myStyle}>See All The Pokemon! (INDEX)</h1>
+                <ul>
+                    {
+                    pokemon.map((character, i) => {
+                        let characterName = character.name
+                        return (
+                      <li>
+                        {/* key = {i} */}
+                        The{' '}
+                        <a href={`/pokemon/${i}`}>
+                        {/* charAt(0).toUpperCase() = allow us to capitalize the first letter of the word */}
+                   {
+                   characterName.charAt(0).toUpperCase() + characterName.slice(1).toLowerCase()
+                   }
+                   
+                        </a>
+                      </li>
+                        )
+                    }) 
+                        
+                    }
 
-                </div>
-            )
+
+
+                </ul>
+            </div>
+        )
     }
 }
 
